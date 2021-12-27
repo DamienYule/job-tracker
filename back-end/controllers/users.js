661ed953ba51a7b1faf67aafaeb4c3ffa89961a5
DamienYule <damienyule@gmail.com>
2021-12-27 17:38:22 -0500

    auth architecture

--- back-end/controllers/users.js
+++ back-end/controllers/users.js
@@ -6,3 +6,18 @@ users.get("/",getAllUsers )
 users.post("/",createUser)
 
 module.exports = users
+
+
+
+// if admin with specific uid you will be able to...
+//     * create, edit and delete job
+//     * mark job status
+//     * 
+//     * 
+// each job will have a uid after being asigned  
+// any uid will ...
+//     * see, comment on job. 
+//     * edit only their comments
+//     * mark status on job specific to them
+//     * asign a job to themselves if job has not been assigned 
+
