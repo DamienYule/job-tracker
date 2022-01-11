710255b58c771ec76245fe1019c9d019af0c8300
DamienYule <damienyule@gmail.com>
2022-01-11 15:41:38 -0500

    heroku deployment

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,37 +1,40 @@
 -- DROP DATABASE IF EXISTS pma_jobs_dev;
 -- CREATE DATABASE pma_jobs_dev;
-\c pma_jobs_dev;
+-- \c pma_jobs_dev;
 
 
 
--- DROP TABLE IF EXISTS pma_jobs;
+DROP TABLE IF EXISTS pma_jobs;
+DROP TABLE IF EXISTS job_comments;
 
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS users;
 
-CREATE TABLE users(
-    id VARCHAR PRIMARY KEY,
-    email VARCHAR
+-- CREATE TABLE users(
+--     id VARCHAR PRIMARY KEY,
+--     email VARCHAR
+-- );
+
+
+
+CREATE TABLE pma_jobs (
+    id SERIAL PRIMARY KEY, 
+    job_name TEXT NOT NULL,
+    description TEXT NOT NULL, 
+    location TEXT NOT NULL, 
+    number_of_hours INT,
+    status TEXT NOT NULL,
+    uid TEXT,
+    owner TEXT,
+    completed BOOLEAN NOT NULL DEFAULT FALSE
 );
-
-
-
--- CREATE TABLE pma_jobs (
---     id SERIAL PRIMARY KEY, 
---     job_name TEXT NOT NULL,
---     description TEXT NOT NULL, 
---     location TEXT NOT NULL, 
---     number_of_hours INT,
---     status TEXT NOT NULL,
---     uid TEXT,
---     completed BOOLEAN NOT NULL DEFAULT FALSE
--- );
--- CREATE TABLE job_comments(
---     id SERIAL PRIMARY KEY,
---     title TEXT NOT NULL,
---     reviewer TEXT NOT NULL,
---     content TEXT NOT NULL,
---     date TEXT NOT NULL,
---     uid TEXT,
---     pma_jobs_id INT REFERENCES pma_jobs (id) 
---         ON DELETE CASCADE 
--- );
+CREATE TABLE job_comments(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    reviewer TEXT NOT NULL,
+    content TEXT NOT NULL,
+    date TEXT NOT NULL,
+    uid TEXT,
+    owner TEXT,
+    pma_jobs_id INT REFERENCES pma_jobs (id) 
+        ON DELETE CASCADE 
+);
